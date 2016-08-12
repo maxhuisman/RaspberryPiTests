f4ac2441baef4ca387006e2e3cde8f472e24070c
max.huisman <max.huisman@gmail.com>
2016-08-12 14:59:07 -0400

    newLED added

---
+++ newLED.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+for x in range(25,29)
+	print "setting pin %d" % (x)
+	GPIO.setup(x,GPIO.OUT)
+for x in range(25,29)
+	print "LED %d on" % (x)
+	GPIO.output(x,GPIO.HIGH)
+	time.sleep(1)
+	GPIO.output(x,GPIO.LOW)
+	print "LED %d off" % (x)
+	time.sleep(1)
+print("Done.")
