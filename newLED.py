07553cafec59d649b6be9bfc4ebcb95a5f20ff76
max.huisman <max.huisman@gmail.com>
2016-08-12 16:52:26 -0400

    fix

--- newLED.py
+++ newLED.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-for x in range(25,29)
+for x in range(25,29):
 	print "setting pin %d" % (x)
 	GPIO.setup(x,GPIO.OUT)
-for x in range(25,29)
+for x in range(25,29):
 	print "LED %d on" % (x)
 	GPIO.output(x,GPIO.HIGH)
 	time.sleep(1)
